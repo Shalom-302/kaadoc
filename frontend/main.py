482969b49f742afa9653df633877c8283828f43f
Shalom-302 <54105227+Shalom-302@users.noreply.github.com>
2025-02-04 17:47:26 +0000

    using docling

--- frontend/main.py
+++ frontend/main.py
@@ -1,30 +1,67 @@
-import streamlit as st
-from components import set_page_config, navbar, upload_file, extraction_options, apply_custom_css
+import json
+import logging
+import time
+import logging
+from pathlib import Path
+from docling.backend.pypdfium2_backend import PyPdfiumDocumentBackend
+from docling.datamodel.base_models import InputFormat
+
+from docling.document_converter import (
+    DocumentConverter,
+    PdfFormatOption,
+    WordFormatOption,
+)
+from docling.pipeline.simple_pipeline import SimplePipeline
+from docling.pipeline.standard_pdf_pipeline import StandardPdfPipeline
+_log = logging.getLogger(__name__)
 
 def main():
-    set_page_config()
-    apply_custom_css()
-    navbar()
+    input_paths = [
+        Path("/Users/macbookkaanari/Downloads/645e0b3c0d6f365e0a214809_exemple-nouveau-permis-francais.jpg"),
+    ]
 
-    menu = ["🏠 Accueil", "🔍 Extraction", "📥 Téléchargement"]
-    choice = st.sidebar.radio("📌 Navigation", menu)
+    doc_converter = (
+        DocumentConverter(  # all of the below is optional, has internal defaults.
+            allowed_formats=[
+                InputFormat.PDF,
+                InputFormat.IMAGE,
+                InputFormat.DOCX,
+                InputFormat.HTML,
+                InputFormat.PPTX,
+                InputFormat.ASCIIDOC,
+                InputFormat.MD,
+            ],  # whitelist formats, non-matching files are ignored.
+            format_options={
+                InputFormat.PDF: PdfFormatOption(
+                    pipeline_cls=StandardPdfPipeline, backend=PyPdfiumDocumentBackend
+                ),
+                InputFormat.DOCX: WordFormatOption(
+                    pipeline_cls=SimplePipeline  # , backend=MsWordDocumentBackend
+                ),
+            },
+        )
+    )
+    start_time = time.time()
+    conv_results = doc_converter.convert_all(input_paths)
 
-    if choice == "🏠 Accueil":
-        st.title("🏠 Bienvenue sur l'application")
-        st.write("Téléversez un fichier et commencez l'extraction.")
 
-    elif choice == "🔍 Extraction":
-        st.title("🔍 Extraction des Données")
-        uploaded_file = upload_file()
+    for res in conv_results:
+        out_path = Path("/Users/macbookkaanari/Documents/kaadoc/data/out/out_json")
+        print(
+            f"Document {res.input.file.name} converted."
+            f"\nSaved JSON output to: {str(out_path)}"
+        )
+        _log.debug(f"Exported text:\n{res.document._export_to_indented_text(max_text_len=16)}")
+        
+        with (out_path / f"{res.input.file.stem}.json").open("w") as fp:
+            fp.write(json.dumps(res.document.export_to_dict(), indent=4))  # Indentation de 4 espaces
 
-        if uploaded_file:
-            extraction_type = extraction_options()
-            st.success(f"Mode d'extraction sélectionné : {extraction_type}")
-            st.write("L'extraction va commencer...")
+    end_time = time.time() - start_time
 
-    elif choice == "📥 Téléchargement":
-        st.title("📥 Téléchargement")
-        st.write("Ici, vous pourrez récupérer vos fichiers convertis en JSON/CSV.")
+    _log.info(f"Documents converted in {end_time:.2f} seconds.")
 
 if __name__ == "__main__":
     main()
+
+
+
